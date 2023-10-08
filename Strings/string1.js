0bfd0294ea322ae2102fd99894b0ef4a1a92dcd7
Tanmay Yadav <145816497+Tanmayyp01@users.noreply.github.com>
2023-10-08 19:18:47 +0530

    Add files via upload

---
+++ Strings/string1.js
@@ -0,0 +1,25 @@
+// Strings :
+
+let fname = "Mahesh" // String Described.
+const hobby ="Swimming"
+
+// Methids to get output :
+
+console.log(fname + hobby) // Old Method
+console.log(`My name is ${fname} and my hobby is ${hobby}`);
+
+// Make all characters to Uppercase :
+console.log(fname.toUpperCase());
+
+// Make all characters to Lowercase :
+console.log(hobby.toLowerCase());
+
+// To get exact length of the string :
+console.log(fname.length); // Output - 6
+console.log(hobby.length); //Output - 8
+
+// Cutting(trim) String : 
+const newString = "    Prince zozo     "
+console.log(newString); // normal output
+console.log(newString.trim()); // trimmed output
+
